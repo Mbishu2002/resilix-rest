7b01a46f886a466e45dbbfccf42aa0395645f981
mbishu <47054530+Mbishu2002@users.noreply.github.com>
2024-07-07 22:57:57 +0100

    Update routing.py

--- main/routing.py
+++ main/routing.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import consumers
 
 urlpatterns = [
-    path('ws/alerts/', consumers.AlertConsumer.as_asgi()),  
+    path('ws/alerts/', consumers.AlertConsumer.as_asgi()),
 ]
